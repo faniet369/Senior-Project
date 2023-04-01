bf4aac2d652284f48b752c78773cbf6e142d9019
Faniet <suthima.so@ku.th>
2023-04-01 21:41:37 +0700

    Update HeartCollector.cs

--- Wizard game/Assets/Scripts/Item/Item_manager/HeartCollector.cs
+++ Wizard game/Assets/Scripts/Item/Item_manager/HeartCollector.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class HeartCollector : MonoBehaviour
 {
-    //private Health health;
     [SerializeField] private AudioSource collectionSoundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision) {
